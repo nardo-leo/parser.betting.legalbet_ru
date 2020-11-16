2e5afc4f91ccb5768656fd338cd5d71815e7d931
nardo-leo <jora.karuzo@gmail.com>
2020-11-16 20:10:27 +0300

    Parse all events in month

--- scrapper.py
+++ scrapper.py
@@ -24,29 +24,39 @@ def scrap():
 
     # parse every day
     for day in days:
+
         koef_list = []
-
-        # TODO remove \n and name of the day
         date = day.find('div', {'class': 'heading-3'}).text.split('\n')
+        game_box = day.findAll('tr', {'class': 'td-row'})
 
-        game_box = day.find('tr', {'class': 'td-row'})
-        koefs = game_box.find_all('td', {'class': 'odd-td'})
-        for koef in koefs:
-            koef_val = koef.find('a', {'class': 'button'}).text.strip('\n')
-            if koef_val == '—':
-                koef_val = ''
-            # slice breaking string
-            koef_list.append(koef_val)
+        # TODO one more loop for all event per day
+        for game_row in game_box:
 
-        game = game_box.find('a', {'class': 'link'}).find_all('span')
-        try:
-            # slice to remove "-" symbol
-            owner = game[0].text[:-2]
-            guest = game[1].text
-            list_of_games.append(f'{date[0]},{owner},{guest},{koef_list[0]},'
-                                 f'{koef_list[1]},{koef_list[2]}')
-        except Exception:
-            pass
+            game_link = game_row.find('a', {'class': 'link'})
+            try:
+                game = game_link.findAll('span')
+            except Exception:
+                game = ['', '']
+            koef_row = game_row.findAll('td', {'class': 'odd-td'})
+
+            for koef in koef_row:
+                # find value
+                try:
+                    koef_val_raw = koef.find('a', {'class': 'button'}).text
+                    koef_val = koef_val_raw.strip('\n')
+                except Exception:
+                    koef_val = ''
+                koef_list.append(koef_val)
+
+            try:
+                # slice to remove "-" symbol
+                owner = game[0].text[:-2]
+                guest = game[1].text
+                list_of_games.append(f'{date[0]},{owner},{guest},'
+                                     f'{koef_list[0]},{koef_list[1]},'
+                                     f'{koef_list[2]}')
+            except Exception:
+                pass
 
     return '\n'.join(list_of_games)
 
